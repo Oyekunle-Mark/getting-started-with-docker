3e89b19b32238f628bba3365ce33c21a22779cf9
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-13 21:21:56 +0100

    Tested out volumes

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const server = require('express')();
 const PORT = process.env.PORT;
 
 server.get('/', (req, res) => res.status(200).json('Hello Docker!'));
+server.get('/volume', (req, res) => res.status(200).json('Hello volumes'));
+server.get('/docker', (req, res) => res.status(200).json('Hello live reload!'));
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
